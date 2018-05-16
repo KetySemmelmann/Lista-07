21caa03dda90467b0aff45cf82d7abdce7e35fe0
KetySemmelmann <39347558+KetySemmelmann@users.noreply.github.com>
2018-05-16 16:34:07 -0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+
+            for (int i = 85; i <= 907; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + " - É par");
+                    soma = soma + i;
+                    Console.WriteLine("A soma dos valores é: {0}", soma);
+                }
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Escreva um algoritmo que calcule a média dos números digitados pelo
+             * usuário, se eles forem pares. Termine a leitura se o usuário digitar zero(0*/
+            int num = 0,soma = 0, media = 0;
+            
+            do
+            {
+               
+                Console.Write(num + " - Digite um número: ");
+                num = Convert.ToInt16(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine(num + " - É par");
+                    soma = soma + num;
+                    media = soma / num;
+                    Console.WriteLine("A média dos valores é: {0}", media);
+                }
+
+            } while (num != 0s);
+               
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
