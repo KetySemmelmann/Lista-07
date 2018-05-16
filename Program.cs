b506888ab65bd97a28a6e02ab75ce6da7d0c1600
KetySemmelmann <39347558+KetySemmelmann@users.noreply.github.com>
2018-05-16 16:32:25 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,34 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            /*Escreva um algoritmo que calcule a média dos números digitados pelo
-             * usuário, se eles forem pares. Termine a leitura se o usuário digitar zero(0*/
-            int num = 0,soma = 0, media = 0;
-            
-            do
+            for (int i = 0; i <= 1000; i = i + 1)
             {
-               
-                Console.Write(num + " - Digite um número: ");
-                num = Convert.ToInt16(Console.ReadLine());
-
-                if (num % 2 == 0)
+                if (i % 7 == 0)
                 {
-                    Console.WriteLine(num + " - É par");
-                    soma = soma + num;
-                    media = soma / num;
-                    Console.WriteLine("A média dos valores é: {0}", media);
+                    Console.WriteLine( i + " - É múltiplo de 7");
                 }
-
-            } while (num != 0s);
-               
-
 
             }
 
